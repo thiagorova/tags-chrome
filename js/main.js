4f4ac85307d10eb5e6df8c3ea3d19fd8708ffc69
Thiago <thiagorova@gmail.com>
2016-12-23 17:04:02 -0200

    updating system

--- js/main.js
+++ js/main.js
@@ -1,1 +1,113 @@
-function setTags(a){for(var b=a.length,c="",d=0;b>d;d++)""!==a[d].tag&&(c=""===c?a[d].tag:c+","+a[d].tag);$("#tags").val(c),fill_tags()}var tagsQuotaReached;$(document).ready(function(){var a=new Clipboard(".copy");a.on("success",function(a){$(a.trigger).find(".action-name").html("Copied!"),window.setTimeout(function(){$(a.trigger).find(".action-name").html("Copy")},1e3),a.clearSelection()}),tagsQuotaReached=!1}),$(document).on("click",".delete-tag",function(a){a.preventDefault();var b=$(this).prev().html(),c=$("#tags").val();0===c.indexOf(b)?$("#tags").val(c.replace(b+",","")):$("#tags").val(c.replace(","+b,"")),tags.excludeTag(b,function(a){alert(a)}),$(this).closest(".tag").remove(),fill_tags()}),$(document).on("click","#get-tags",function(a){tagsQuotaReached===!1?($(".explanation").addClass("hide"),$("#loading").removeClass("hide"),chrome.tabs.query({active:!0,currentWindow:!0},function(a){chrome.tabs.sendMessage(a[0].id,{please:"get tags"},function(a){if("undefined"==typeof a);else if(""===a.error){var b=new Tags("eJwVjDsKgDAUwE5Uef/P5lWKQy1YCiKe37pkCCEOGcCMBCQMjAqhKihq8Et2onAupOgJqZEYmEarIMMFASUTC1Rfn/KcvbZ5z7fubdR+bcccHxBTF9s=");b.setText(a.text),b.getTagsFull(!1,function(a){$("#loading").addClass("hide"),setTags(a)},function(a){$("#loading").addClass("hide"),$(".explanation").removeClass("hide"),$(".explanation").text(a),$("#get-tags").text("UGRADE TO PREMIUM"),$(".explanation").css("background-color","#FFDBDB"),$(".explanation").css("color","#666"),"Monthly quota reached. Please upgrade to an premium account"===a&&(tagsQuotaReached=!0)})}else alert(a.error)})})):chrome.tabs.create({url:"http://www.tags.authorship.me"})});var fill_tags=function(){var a=$("#tags").val().split(",");$(".tags-container").empty(),$.each(a,function(a,b){$(".tags-container").append("<div class='tag'><a href='#'>"+b+"</a><a class='hide delete-tag' href='#'><i class='fa fa-times-circle' aria-hidden='true'></i></a></div>")}),$("#number-of-tags").html("Number of tags: "+a.length.toString())};$(document).on("click",".email",function(){$(this).attr("href","mailto:?subject="+encodeURIComponent("Tags")+"&body="+encodeURIComponent($("#tags").val()))}),$(document).on("click",".csv",function(){var b=($("#tags").val(),[[$("#tags").val()]]),c="data:text/csv;charset=utf-8,";b.forEach(function(a,d){dataString=a.join(","),c+=d<b.length?dataString+"\n":dataString});var d=encodeURI(c);window.open(d)});
+var tagsQuotaReached;
+var tags;
+
+  chrome.storage.local.get('apikey', function(storedItem) {
+    if(jQuery.isEmptyObject(storedItem) === false) {
+      tags = new Tags(storedItem.apikey);
+    } else {
+      window.location.href = "setKey.html"
+    }
+  });
+
+
+$(document).ready(function() {
+  var clipboard = new Clipboard('.copy');
+  clipboard.on('success', function(e) {
+    $(e.trigger).find('.action-name').html('Copied!');
+    window.setTimeout(function(){
+      $(e.trigger).find('.action-name').html('Copy');
+    },1000);
+    e.clearSelection();
+  });
+  
+  tagsQuotaReached = false
+});
+ 
+$(document).on('click','.delete-tag',function(e){
+  e.preventDefault();
+
+  var t = $(this).prev().html();
+  var f = $("#tags").val();
+
+  if( f.indexOf( t ) === 0 ) $("#tags").val( f.replace(t+',','') );
+  else $("#tags").val( f.replace(','+t,'') );
+  tags.excludeTag(t);
+  $(this).closest('.tag').remove();
+
+  fill_tags();
+});
+
+$(document).on("click", "#get-tags", function(e) {
+  if (tagsQuotaReached === false) {
+    $(".explanation").addClass("hide"), $("#loading").removeClass("hide");
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {please: "get tags"}, function(response) {
+        if  (response.error === "") {
+          tags.setText(response.text);
+//          tags.getTagsFull(true,  function(tagResponse) {		//this eliminates tags with too little relevance, including previously excluded tags...
+          tags.getTagsFull(false,  function(tagResponse) {		
+            $("#loading").addClass("hide");
+            setTags(tagResponse);
+          }, function (response) {
+            $("#loading").addClass("hide"); 
+            $(".explanation").removeClass("hide"); 
+            $(".explanation").text(response); 
+            $("#get-tags").text("UGRADE TO PREMIUM"); 
+            $(".explanation").css("background-color", "#FFDBDB");
+             $(".explanation").css("color", "#666");
+             if(response === "Monthly quota reached. Please upgrade to an premium account") {  
+               tagsQuotaReached = true;
+             }
+          });
+        } else {
+          alert(response.error);
+        }
+      });
+    });
+  } else {
+    chrome.tabs.create({
+      url: "http://www.tags.authorship.me"
+    });
+  }
+});
+
+$(document).on("click", ".email", function() {
+  $(this).attr("href", "mailto:?subject=" + encodeURIComponent("Tags") + "&body=" + encodeURIComponent($("#tags").val()))
+});
+
+$(document).on("click", ".csv", function() {
+  var b = ($("#tags").val(), [
+      [$("#tags").val()]
+    ]),
+    c = "data:text/csv;charset=utf-8,";
+  b.forEach(function(a, d) {
+    dataString = a.join(","), c += d < b.length ? dataString + "\n" : dataString
+  });
+  var d = encodeURI(c);
+  window.open(d)
+});
+
+function setTags (tagsArr) {
+  var length = tagsArr.length;
+  var tagsString = "";
+
+  for (var i = 0; i < length; i++) {
+    if(tagsArr[i].tag !=="") {
+      if(tagsString === "") tagsString = tagsArr[i].tag;
+      else tagsString = tagsString + "," + tagsArr[i].tag;
+    }
+  }
+  $('#tags').val(tagsString);
+  fill_tags();
+}
+
+var fill_tags = function(){
+
+  var tagsArray = $('#tags').val().split(',');
+  $('.tags-container').empty();
+  $.each( tagsArray, function( i , v ){
+    $('.tags-container').append("<div class='tag'><a href='#'>"+v+"</a><a class='hide delete-tag' href='#'><i class='fa fa-times-circle' aria-hidden='true'></i></a></div>");
+  });
+
+  $('#number-of-tags').html("Number of tags: " + ( tagsArray.length.toString() ) );
+};
---
+++ js/setKey.js
@@ -0,0 +1,49 @@
+  var login = function(e) {
+    if(e.keyCode !== 13 && e.currentTarget !== document.getElementById("login"))  return;  //accept on Enter key presses or lgin button pushed
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    data = {
+      user: {
+        "email": email, 
+        "password": password
+      },
+      tool: "tags"
+    }
+    testLogin(data);
+  }
+  
+    var createXHTTP = function(callback) {
+        var xhttp;
+        if (window.XMLHttpRequest) {
+           xhttp = new XMLHttpRequest();
+        } else {
+           xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xhttp.onreadystatechange = callback;
+        return xhttp;
+    };
+
+//a few helper functions
+    var testLogin = function(data) {
+         var xhttp = createXHTTP( function(response) {
+             if (xhttp.readyState == 4) {
+                 if (xhttp.status == 200) {
+                     chrome.storage.local.set({ 'apikey': JSON.parse(xhttp.responseText).key });
+          	          window.location.href = "tags.html"
+                 } else if (xhttp.status == 401) {
+	           document.getElementById("message").innerHTML = "login invalid";
+	      }
+            }
+        });
+        xhttp.open('POST', 'http://metrics.authorship.me/users/login', true);
+        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhttp.setRequestHeader("Origin", window.top.location.href.split("?")[0]);
+        xhttp.send(JSON.stringify( data ));
+    };
+
+window.onload=function(){
+    document.getElementById("login").addEventListener('click', login);
+    document.getElementById("password").addEventListener('keypress', login);
+}
+  
+  
